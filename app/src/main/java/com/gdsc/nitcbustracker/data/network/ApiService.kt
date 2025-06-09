73a28836960c6961101e63cf4da9319fcfd5e621
gamehackerever <gamehackerever@gmail.com>
2025-06-09 14:20:13 +0530

    app: Setup the registation page - Only shows on initial login using NITC mail

--- app/src/main/java/com/gdsc/nitcbustracker/data/network/ApiService.kt
+++ app/src/main/java/com/gdsc/nitcbustracker/data/network/ApiService.kt
@@ -1,7 +1,9 @@
 package com.gdsc.nitcbustracker.data.network
 
+import com.gdsc.nitcbustracker.data.model.GenericResponse
 import com.gdsc.nitcbustracker.data.model.LoginRequest
 import com.gdsc.nitcbustracker.data.model.LoginResponse
+import com.gdsc.nitcbustracker.data.model.RegisterRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -14,6 +16,9 @@ interface ApiService {
 
     @POST("/api/user/partial-registration")
     suspend fun partialRegistration(@Body request: Map<String, String>): Response<Map<String, Any>>
+
+    @POST("/api/user/complete-registration")
+    suspend fun completeRegistration(@Body request: RegisterRequest): Response<GenericResponse>
 
     @GET("/api/user/exists")
     suspend fun checkUserExists(@Query("email") email: String): Response<Map<String, Boolean>>
