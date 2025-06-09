6300c1155f3f89d4f7b4d170f368ce510afa6418
gamehackerever <gamehackerever@gmail.com>
2025-06-09 11:22:29 +0530

    app: Initialize app base - Create Login Activity

---
+++ app/src/main/java/com/gdsc/nitcbustracker/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.gdsc.nitcbustracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
