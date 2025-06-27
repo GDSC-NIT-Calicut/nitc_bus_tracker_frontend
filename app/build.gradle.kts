ca91b46e53c18a574a307e4ab0982f27315cd1f1
gamehackerever <gamehackerever@gmail.com>
2025-06-27 19:01:10 +0100

    app: Bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.gdsc.nitcbustracker"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resValue("string", "google_maps_key", mapsApiKey)
